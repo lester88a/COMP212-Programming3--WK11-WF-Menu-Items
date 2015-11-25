aaea38a3578ec6e9691db050adfad672f80bc1d3
lester88a <lester88a@gmail.com>
2015-11-25 09:36:31 -0500

    Basic

---
+++ MenuClassWork/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace MenuClassWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearFontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timesNewRomanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.couriesrNewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelDisplay = new System.Windows.Forms.Label();
+            this.boldToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.italicToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redRadioButton = new System.Windows.Forms.RadioButton();
+            this.greenRadioButton = new System.Windows.Forms.RadioButton();
+            this.blackRadioButton = new System.Windows.Forms.RadioButton();
+            this.checkBoxTimesNewRoman = new System.Windows.Forms.CheckBox();
+            this.checkBoxCourier = new System.Windows.Forms.CheckBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.aboutButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.formatToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearFontToolStripMenuItem,
+            this.clearColorToolStripMenuItem});
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.formatToolStripMenuItem.Text = "Format";
+            // 
+            // clearFontToolStripMenuItem
+            // 
+            this.clearFontToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.timesNewRomanToolStripMenuItem,
+            this.couriesrNewToolStripMenuItem,
+            this.boldToolStripMenuItem,
+            this.italicToolStripMenuItem});
+            this.clearFontToolStripMenuItem.Name = "clearFontToolStripMenuItem";
+            this.clearFontToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearFontToolStripMenuItem.Text = "Font";
+            // 
+            // timesNewRomanToolStripMenuItem
+            // 
+            this.timesNewRomanToolStripMenuItem.Name = "timesNewRomanToolStripMenuItem";
+            this.timesNewRomanToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.timesNewRomanToolStripMenuItem.Text = "Times New Roman";
+            this.timesNewRomanToolStripMenuItem.Click += new System.EventHandler(this.timesNewRomanToolStripMenuItem_Click);
+            // 
+            // couriesrNewToolStripMenuItem
+            // 
+            this.couriesrNewToolStripMenuItem.Name = "couriesrNewToolStripMenuItem";
+            this.couriesrNewToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.couriesrNewToolStripMenuItem.Text = "Couriesr New";
+            this.couriesrNewToolStripMenuItem.Click += new System.EventHandler(this.couriesrNewToolStripMenuItem_Click);
+            // 
+            // clearColorToolStripMenuItem
+            // 
+            this.clearColorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.blackToolStripMenuItem,
+            this.greenToolStripMenuItem,
+            this.redToolStripMenuItem});
+            this.clearColorToolStripMenuItem.Name = "clearColorToolStripMenuItem";
+            this.clearColorToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearColorToolStripMenuItem.Text = "Color";
+            // 
+            // blackToolStripMenuItem
+            // 
+            this.blackToolStripMenuItem.Name = "blackToolStripMenuItem";
+            this.blackToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.blackToolStripMenuItem.Text = "Black";
+            this.blackToolStripMenuItem.Click += new System.EventHandler(this.blackToolStripMenuItem_Click);
+            // 
+            // greenToolStripMenuItem
+            // 
+            this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
+            this.greenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.greenToolStripMenuItem.Text = "Green";
+            this.greenToolStripMenuItem.Click += new System.EventHandler(this.greenToolStripMenuItem_Click);
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // labelDisplay
+            // 
+            this.labelDisplay.AutoSize = true;
+            this.labelDisplay.Location = new System.Drawing.Point(59, 113);
+            this.labelDisplay.Name = "labelDisplay";
+            this.labelDisplay.Size = new System.Drawing.Size(65, 13);
+            this.labelDisplay.TabIndex = 1;
+            this.labelDisplay.Text = "Hello World!";
+            // 
+            // boldToolStripMenuItem
+            // 
+            this.boldToolStripMenuItem.Name = "boldToolStripMenuItem";
+            this.boldToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.boldToolStripMenuItem.Text = "Bold";
+            this.boldToolStripMenuItem.Click += new System.EventHandler(this.boldToolStripMenuItem_Click);
+            // 
+            // italicToolStripMenuItem
+            // 
+            this.italicToolStripMenuItem.Name = "italicToolStripMenuItem";
+            this.italicToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.italicToolStripMenuItem.Text = "Italic";
+            this.italicToolStripMenuItem.Click += new System.EventHandler(this.italicToolStripMenuItem_Click);
+            // 
+            // redRadioButton
+            // 
+            this.redRadioButton.AutoSize = true;
+            this.redRadioButton.Location = new System.Drawing.Point(169, 129);
+            this.redRadioButton.Name = "redRadioButton";
+            this.redRadioButton.Size = new System.Drawing.Size(45, 17);
+            this.redRadioButton.TabIndex = 2;
+            this.redRadioButton.TabStop = true;
+            this.redRadioButton.Text = "Red";
+            this.redRadioButton.UseVisualStyleBackColor = true;
+            this.redRadioButton.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // greenRadioButton
+            // 
+            this.greenRadioButton.AutoSize = true;
+            this.greenRadioButton.Location = new System.Drawing.Point(169, 152);
+            this.greenRadioButton.Name = "greenRadioButton";
+            this.greenRadioButton.Size = new System.Drawing.Size(54, 17);
+            this.greenRadioButton.TabIndex = 3;
+            this.greenRadioButton.TabStop = true;
+            this.greenRadioButton.Text = "Green";
+            this.greenRadioButton.UseVisualStyleBackColor = true;
+            this.greenRadioButton.Click += new System.EventHandler(this.greenToolStripMenuItem_Click);
+            // 
+            // blackRadioButton
+            // 
+            this.blackRadioButton.AutoSize = true;
+            this.blackRadioButton.Location = new System.Drawing.Point(169, 180);
+            this.blackRadioButton.Name = "blackRadioButton";
+            this.blackRadioButton.Size = new System.Drawing.Size(52, 17);
+            this.blackRadioButton.TabIndex = 4;
+            this.blackRadioButton.TabStop = true;
+            this.blackRadioButton.Text = "Black";
+            this.blackRadioButton.UseVisualStyleBackColor = true;
+            this.blackRadioButton.Click += new System.EventHandler(this.blackToolStripMenuItem_Click);
+            // 
+            // checkBoxTimesNewRoman
+            // 
+            this.checkBoxTimesNewRoman.AutoSize = true;
+            this.checkBoxTimesNewRoman.Location = new System.Drawing.Point(141, 37);
+            this.checkBoxTimesNewRoman.Name = "checkBoxTimesNewRoman";
+            this.checkBoxTimesNewRoman.Size = new System.Drawing.Size(116, 17);
+            this.checkBoxTimesNewRoman.TabIndex = 5;
+            this.checkBoxTimesNewRoman.Text = "Times New Roman";
+            this.checkBoxTimesNewRoman.UseVisualStyleBackColor = true;
+            this.checkBoxTimesNewRoman.Click += new System.EventHandler(this.timesNewRomanToolStripMenuItem_Click);
+            // 
+            // checkBoxCourier
+            // 
+            this.checkBoxCourier.AutoSize = true;
+            this.checkBoxCourier.Location = new System.Drawing.Point(141, 60);
+            this.checkBoxCourier.Name = "checkBoxCourier";
+            this.checkBoxCourier.Size = new System.Drawing.Size(59, 17);
+            this.checkBoxCourier.TabIndex = 6;
+            this.checkBoxCourier.Text = "Courier";
+            this.checkBoxCourier.UseVisualStyleBackColor = true;
+            this.checkBoxCourier.Click += new System.EventHandler(this.couriesrNewToolStripMenuItem_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(35, 207);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutButton
+            // 
+            this.aboutButton.Location = new System.Drawing.Point(124, 206);
+            this.aboutButton.Name = "aboutButton";
+            this.aboutButton.Size = new System.Drawing.Size(75, 23);
+            this.aboutButton.TabIndex = 8;
+            this.aboutButton.Text = "About";
+            this.aboutButton.UseVisualStyleBackColor = true;
+            this.aboutButton.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.aboutButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.checkBoxCourier);
+            this.Controls.Add(this.checkBoxTimesNewRoman);
+            this.Controls.Add(this.blackRadioButton);
+            this.Controls.Add(this.greenRadioButton);
+            this.Controls.Add(this.redRadioButton);
+            this.Controls.Add(this.labelDisplay);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearFontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timesNewRomanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem couriesrNewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blackToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+        private System.Windows.Forms.Label labelDisplay;
+        private System.Windows.Forms.ToolStripMenuItem boldToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem italicToolStripMenuItem;
+        private System.Windows.Forms.RadioButton redRadioButton;
+        private System.Windows.Forms.RadioButton greenRadioButton;
+        private System.Windows.Forms.RadioButton blackRadioButton;
+        private System.Windows.Forms.CheckBox checkBoxTimesNewRoman;
+        private System.Windows.Forms.CheckBox checkBoxCourier;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button aboutButton;
+    }
+}
+
---
+++ MenuClassWork/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuClassWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is an example of how to use Menu Items", "About", 
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ClearFont()
+        {
+            timesNewRomanToolStripMenuItem.Checked = false;
+            
+        }
+        private void ClearColor()
+        {
+            blackToolStripMenuItem.Checked = false;
+            greenToolStripMenuItem.Checked = false;
+            redToolStripMenuItem.Checked = false;
+        }
+
+        private void blackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearColor();
+            labelDisplay.ForeColor = Color.Black;
+            blackToolStripMenuItem.Checked = true;
+        }
+
+        private void greenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearColor();
+            labelDisplay.ForeColor = Color.Green;
+            greenToolStripMenuItem.Checked = true;
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearColor();
+            labelDisplay.ForeColor = Color.Red;
+            redToolStripMenuItem.Checked = true;
+            
+        }
+
+        private void timesNewRomanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearFont();
+            timesNewRomanToolStripMenuItem.Checked = true;
+            labelDisplay.Font = new Font("Times New Roman", 14, labelDisplay.Font.Style);
+        }
+
+        private void couriesrNewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearFont();
+            couriesrNewToolStripMenuItem.Checked = true;
+            labelDisplay.Font = new Font("Courier", 14, labelDisplay.Font.Style);
+        }
+
+        private void boldToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearFont();
+            boldToolStripMenuItem.Checked = !boldToolStripMenuItem.Checked;
+            labelDisplay.Font = new Font(labelDisplay.Font, labelDisplay.Font.Style ^ FontStyle.Bold);
+        }
+
+        private void italicToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearFont();
+            italicToolStripMenuItem.Checked = !italicToolStripMenuItem.Checked;
+            labelDisplay.Font = new Font(labelDisplay.Font, labelDisplay.Font.Style ^ FontStyle.Italic);
+        }
+        
+    }
+}
